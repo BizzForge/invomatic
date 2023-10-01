12195fe23f16f585069968b79cac80c6638b0c5a
anwar254 <sadatt254@gmail.com>
2023-10-02 02:21:24 +0300

    inventory button created

--- app/inventory/page.jsx
+++ app/inventory/page.jsx
@@ -1,8 +1,18 @@
+'use client'
 import React from 'react'
 import Console from '../tempates/console/console'
+import ButtonWithIcon from '../components/buttons/button-with-icon/button-with-icon'
+import { PlusIcon } from '@heroicons/react/24/outline';
 
 export default function Inventory() {
+  const handleClick = () => {
+    console.log("Button clicked!");
+  }
   return (
-    <Console>This is the Inventory page</Console>
+    <Console>
+      <div className="p-5">
+        <ButtonWithIcon Icon={PlusIcon} href="/new-inventory" text={'New Inventory'} onClick={handleClick}/>
+      </div>
+    </Console>
   )
 }
