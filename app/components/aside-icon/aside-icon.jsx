201ed828481dff84ddc84e9f6ddbd12098d04d04
anwar254 <sadatt254@gmail.com>
2023-10-09 02:59:35 +0300

    active changed

--- app/components/aside-icon/aside-icon.jsx
+++ app/components/aside-icon/aside-icon.jsx
@@ -4,11 +4,11 @@ import React, { Fragment } from 'react';
 export default function AsideIcon({ Icon, title, isOpen, link, isActive }) {
     return (
         <Link href={link} passHref>
-            <div className={`flex py-3 ${isOpen ? 'justify-left' : 'justify-left'} rounded-lg space-x-[32px] px-2.5 transition-all duration-200 hover:font-bold hover:text-primary hover:bg-main-bg ${isActive ? '!bg-main-bg font-bold text-white' : ''}`} >
+            <div className={`flex py-3 ${isOpen ? 'justify-left' : 'justify-left'} rounded-lg space-x-[32px] px-2.5 transition-all duration-200 hover:font-bold hover:text-primary hover:bg-main-bg ${isActive ? '!bg-primary font-bold text-white' : ''}`} >
                 <div className="w-6 h-6">
-                    <Icon strokeWidth={2} className="md:w-6 md:h-6 text-acc-color" />
+                    <Icon strokeWidth={2} className={`md:w-6 md:h-6 text-acc-color ${isActive ? 'text-white' : ''}`} />
                 </div>
-                {isOpen && <div className='ml-[42px] text-[16px] text-acc-color whitespace-nowrap'>{title}</div>}
+                {isOpen && <div className={`ml-[42px] text-[16px] text-acc-color whitespace-nowrap ${isActive ? 'text-white' : ''}`}>{title}</div>}
             </div>
         </Link>
     );
