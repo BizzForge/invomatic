f18b94135fb31da8ebcaeb27907da939e466f6d6
anwar254 <sadatt254@gmail.com>
2023-10-21 04:12:59 +0300

    product quantity increment

--- app/components/pos-products/pos-products.jsx
+++ app/components/pos-products/pos-products.jsx
@@ -1,26 +1,40 @@
-'use client'
-
 import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function PosProduct() {
-    const products = [
+    const [products, setProducts] = useState([
         {
             title: "Grey Goose",
             subtitle: 750,
-            price: 3240
+            price: 3240,
+            quantity: 1 
         },
-    ]
+        {
+            title: "Grey Goose",
+            subtitle: 750,
+            price: 3240,
+            quantity: 1
+        },
+    ]);
 
-    if (products.length === 0) {
-        return (
-            <div className="pt-8 text-acc-color-2 flex items-center justify-center text-cetner font-bold text-acc-color-2">Choose a cartegory</div>
-        );
+    const qtyIncrement = (index) => {
+        const updatedProducts = [...products];
+        updatedProducts[index].quantity += 1;
+        setProducts(updatedProducts);
     }
-    
+
+    const qtyDecrement = (index) => {
+        const updatedProducts = [...products];
+        if (updatedProducts[index].quantity > 1) {
+            updatedProducts[index].quantity -= 1;
+            setProducts(updatedProducts);
+        }
+    }
+
     return (
       <div className="flex pt-8 flex-wrap -mx-2">
         {products.map((product, index) => (
-          <a key={index} className="w-full cursor-pointer sm:w-1/2 md:w-1/2 lg:w-1/4 px-2 mb-4">
+          <div key={index} className="w-full cursor-pointer sm:w-1/2 md:w-1/2 lg:w-1/4 px-2 mb-4">
             <div className={`font-light bg-white flex flex-col justify-between p-6 h-[140px] rounded-lg shadow`}>
                 <div>
                     <h4 className="text-[18px] text-acc-color-2 font-bold pb-1">{product.title}</h4>
@@ -31,18 +45,18 @@ export default function PosProduct() {
                         <span className="mr-2">{product.subtitle}</span>
                     </p>
                     <div className="flex text-[12px] items-center">
-                        <a className="bg-acc-btn p-1 rounded-sm">
+                        <a onClick={() => qtyIncrement(index)} className="bg-acc-btn p-1 rounded-sm">
                             <PlusIcon className="h-5 w-5 text-acc-color text-[8px]" />
                         </a>
-                        <p className="text-[14px] px-3">2</p>
-                        <a className="bg-acc-btn p-1 rounded-sm">
+                        <p className="text-[14px] px-3">{product.quantity}</p>
+                        <a onClick={() => qtyDecrement(index)} className="bg-acc-btn p-1 rounded-sm">
                             <MinusIcon className="h-5 w-5 text-acc-color text-[8px]" />
                         </a>
                     </div>
                 </div>
             </div>
-          </a>
+          </div>
         ))}
       </div>
     );
-  }
+}
