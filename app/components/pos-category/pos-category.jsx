da384f230dfefabf6dc2e2bf63a35a224c3c31d9
anwar254 <sadatt254@gmail.com>
2023-10-21 04:42:19 +0300

    pos category six per row

--- app/components/pos-category/pos-category.jsx
+++ app/components/pos-category/pos-category.jsx
@@ -25,15 +25,15 @@ export default function PosCategory() {
         }
     ]
     return (
-      <div className="flex pb-8 border-b-[0.5px] border-acc-color flex-wrap -mx-2">
-        {categories.map((category, index) => (
-          <a key={index} className="w-full cursor-pointer sm:w-1/2 md:w-1/2 lg:w-1/4 px-2 mb-4">
-            <div style={{ background: colors[index % colors.length] }} className={`text-white font-light flex flex-col justify-between p-6 h-[133px] rounded-lg shadow`}>
-              <h4 className="text-[18px] font-bold">{category.title}</h4>
-              <p className="text-[12px]">{category.quantity} items</p>
-            </div>
-          </a>
-        ))}
-      </div>
+        <div className="flex pb-8 border-b-[0.5px] border-acc-color flex-wrap -mx-2">
+            {categories.map((category, index) => (
+            <a key={index} className="w-full cursor-pointer sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-2 mb-4">
+                <div style={{ background: colors[index % colors.length] }} className={`text-white font-light flex flex-col justify-between p-6 h-[133px] rounded-lg shadow`}>
+                <h4 className="text-[18px] font-bold">{category.title}</h4>
+                <p className="text-[12px]">{category.quantity} items</p>
+                </div>
+            </a>
+            ))}
+        </div>
     );
   }
